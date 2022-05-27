d41466dbdab17af20d9d8241b7b0c0d28d494bb6
rawwr <akshayace0302@gmail.com>
2022-05-27 23:52:54 +0530

    links inside routers and header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StreamList />} />
-        <Route path="/streams/create" element={<StreamCreate />} />
-        <Route path="/streams/edit" element={<StreamEdit />} />
-        <Route path="/streams/delete" element={<StreamDelete />} />
-        <Route path="/streams/show" element={<StreamShow />} />
-      </Routes>
-    </Router>
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="/streams/create" element={<StreamCreate />} />
+          <Route path="/streams/edit" element={<StreamEdit />} />
+          <Route path="/streams/delete" element={<StreamDelete />} />
+          <Route path="/streams/show" element={<StreamShow />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
