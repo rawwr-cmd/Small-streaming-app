c10235046c11e9af3343a2b6e41a3f103ae67568
rawwr <akshayace0302@gmail.com>
2022-05-28 14:26:36 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
