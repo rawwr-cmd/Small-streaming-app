48aa679756c16ce7851e87801c0ff652cac842b1
rawwr <akshayace0302@gmail.com>
2022-05-28 14:26:23 +0530

    changing the method name

--- src/components/auth/GoogleAuth.js
+++ src/components/auth/GoogleAuth.js
@@ -23,11 +23,11 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -37,7 +37,10 @@ class GoogleAuth extends Component {
     } else if (this.state.isSignedIn) {
       return (
         <div>
-          <button onClick={this.onSignOut} className="ui red google button">
+          <button
+            onClick={this.onSignOutClick}
+            className="ui red google button"
+          >
             <i className="google icon" />
             Sign Out
           </button>
@@ -46,7 +49,7 @@ class GoogleAuth extends Component {
     } else {
       return (
         <div>
-          <button onClick={this.onSignIn} className="ui red google button">
+          <button onClick={this.onSignInClick} className="ui red google button">
             <i className="google icon" />
             Sign In with Google
           </button>
