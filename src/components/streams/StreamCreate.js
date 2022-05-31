1874a41f83176e99dfa663ff19fde1ffd945c2f3
rawwr <akshayace0302@gmail.com>
2022-06-01 01:51:00 +0530

    showing errors on touch

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,25 +1,37 @@
 import { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreator extends Component {
-  renderList({ input, label }) {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderList = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValues) {
     console.log(formValues);
   }
 
   render() {
-    console.log(this.props);
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderList} label="Enter Title" />
         <Field
@@ -47,4 +59,4 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({ form: "streamCreate" })(StreamCreator);
+export default reduxForm({ form: "streamCreate", validate })(StreamCreator);
