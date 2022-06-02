ffb325c99f919eee3f7720e13f82d439580ae2f4
rawwr <akshayace0302@gmail.com>
2022-06-03 00:13:30 +0530

    deleting properties w omit and merging list of records

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import {
   CREATE_STREAM,
   FETCH_STREAMS,
@@ -8,6 +10,9 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._mapKeys(action.payload, "id") };
+
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
 
@@ -16,6 +21,9 @@ export default (state = {}, action) => {
 
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
 
     default:
       return state;
