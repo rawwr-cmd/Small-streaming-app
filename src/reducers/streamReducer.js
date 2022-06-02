fdcdb28b4ded0a21e4569f8cc8e6c699e94bdedd
rawwr <akshayace0302@gmail.com>
2022-06-03 00:13:50 +0530

    fixing a typo

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -11,7 +11,7 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return { ...state, ..._mapKeys(action.payload, "id") };
+      return { ...state, ..._.mapKeys(action.payload, "id") };
 
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
