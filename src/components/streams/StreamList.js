3178621ff1168f372849bf5b33b6ca7799dd1b7e
rawwr <akshayace0302@gmail.com>
2022-06-03 01:43:39 +0530

    rendering all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,35 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>StreamList</div>;
+    // console.log(this.props.streams);
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  //object to arrays
+  // console.log(Object.values(state.streams));
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
