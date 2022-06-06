df5ead798369b8418b197f5b6050d19e927b9fa2
rawwr <akshayace0302@gmail.com>
2022-06-06 16:10:56 +0530

    refracting stream creation

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { fetchStream } from "../../actions";
+import { useDispatch } from "react-redux";
 
 const StreamEdit = () => {
-  return <div>StreamEdit</div>;
-};
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchStream(id));
+  }, [dispatch, id]);
+
+  const stream = useSelector((state) => state.streams[id]);
+  // console.log(stream);
+
+  const streamDetails = () => {
+    if (!stream) {
+      return <div>Loadings..</div>;
+    }
+    return (
+      <div>
+        <div>{stream.title}</div>
+      </div>
+    );
+  };
+
+  return <div className="ui relaxed divided list">{streamDetails()}</div>;
+};
 export default StreamEdit;
